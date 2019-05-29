6da7c9a7f0b50bf3ae721eae99f8da67ad7db18d
Sanjiccka <44682454+Sanjiccka@users.noreply.github.com>
2019-05-29 18:56:54 +0200

    Add files via upload

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,83 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { Login } from "../Login/Login.js";
-import { Register} from "../Register/Register.js";
+import { Register } from "../Register/Register.js";
 import { Portal } from "../Portal/Portal.js";
 
 
+
+
 export class App extends React.Component {
+
+    constructor(props) {
+        this.state = {
+            products: [],
+            error: {
+                show: false,
+                errorMsg: ""
+            }
+        }
+
+        this.FetchProducts = this.FetchProducts.bind(this);
+        this.CreateNewProduct = this.CreateNewProduct.bind(this);
+
+    }
+
+
+    componentDidMount() {
+        this.props.fetch();
+        this.FetchProducts();
+    }
+
+
+
+
+    // Fetch (GET method)
+    FetchProducts() {
+        fetch(API_ROUTE + "/users")
+            .then(res => { return res.json() })
+            .then(res => this.setState({ products: res }))
+            .catch(err => {
+                this.setState(state => {
+                    return {
+                        error: {
+                            ...state,
+                            show: true,
+                            errorMsg: err
+
+                        }
+                    }
+                })
+            });
+    }
+
+
+    // Fetch (POST method)
+    CreateNewProduct() {
+
+        fetch(url, { method: "POST", data: JSON.stringify(this.state.product) })
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(err => this.setState({ errorMsg: err }))
+    }
+
+
+
+
+
     render() {
         return (
             <main id="app">
-
                 <Route exact path="/" component={Login} />
                 <Route path="/register" component={Register} />
-                <Route path="/products" component={Portal} />
-                <Route path="/newProduct" component={Portal} />
-                <Route path="/expenses" component={Portal} />
-                <Route path="/delete" component={Portal} />
-
-
+                <Portal />
             </main>
 
-            
+
         )
     }
+
 }
+
+
+
--- src/components/Portal/Portal.js
+++ src/components/Portal/Portal.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { NavLink , Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { Header } from "../Header/Header.js";
 import { Products } from "../Products/Products.js";
 import { NewProduct } from "../NewProduct/NewProduct.js";
 import { Expenses } from "../Expenses/Expenses.js";
-import { ProductDelete } from "../ProductDelete/ProductDelete.js";
+
 
 
 export class Portal extends React.Component {
     render () {
         return (
             <section id="portal">
-            <Header />
-            <Route path="/products" component={Products} />
-            <Route path="/newProduct" component={NewProduct} />
-            <Route path="/expenses" component={Expenses} />
-            <Route path="/delete" component={ProductDelete} />
-            </section>
+            <Route exact path="/(newproduct|products|expenses)/" component={Header}/>
+            <Route path = "/newproduct" component = {NewProduct}/>
+            <Route path ="/products" component = {Products}/>
+            <Route path ="/expenses" component = {Expenses}/>
+
+        </section>
         )
     }
-}
+} 
